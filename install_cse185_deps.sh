8fc1f7eabefaa2e994c29b8bf4e064709ba34325
gymreklab <mgymrek@ucsd.edu>
2019-03-27 15:48:23 +0000

    adding week2 deps to docker install file

--- install_cse185_deps.sh
+++ install_cse185_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+##### Week 1 ######
 # Install samtools (needed to index reference fasta files)
 cd /sources
 wget -O samtools-1.9.tar.bz2 https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2
@@ -20,3 +21,11 @@ wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zi
 unzip fastqc_v0.11.8.zip
 chmod 755 FastQC/fastqc
 ln -s /sources/FastQC/fastqc /usr/local/bin/fastqc
+
+###### Week 2 ########
+# Install sickle
+cd /sources
+git clone https://github.com/najoshi/sickle
+cd sickle
+make
+cp sickle /usr/local/bin/sickle
