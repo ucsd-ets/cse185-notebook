c28cd2abc69c0d118bf43a2923e079e99a2ac10f
mgymrek <melissa.gymrek@gmail.com>
2019-04-05 15:29:43 -0700

    updating docker for week 3

--- install_cse185_deps.sh
+++ install_cse185_deps.sh
@@ -92,3 +92,28 @@ cd /sources
 wget http://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20190304.zip
 unzip plink_linux_x86_64_20190304.zip
 cp plink /usr/local/bin
+
+# Install htslib (with tabix)
+cd /sources
+wget -O htslib-1.8.tar.bz2 https://github.com/samtools/htslib/releases/download/1.8/htslib-1.8.tar.bz2
+tar -xjvf htslib-1.8.tar.bz2
+cd htslib-1.8/
+./configure
+make
+make install
+
+# Install bcftools
+cd /sources
+wget -O bcftools-1.9.tar.bz2 https://github.com/samtools/bcftools/releases/download/1.9/bcftools-1.9.tar.bz2
+tar -xjvf bcftools-1.9.tar.bz2
+cd bcftools-1.9
+./configure
+make
+make install
+
+# Install VCFTools
+cd /sources
+wget https://github.com/vcftools/vcftools/releases/download/v0.1.16/vcftools-0.1.16.tar.gz
+tar -xzvf vcftools-0.1.16.tar.gz
+cd vcftools-0.1.16
+./autogen.sh && ./configure && make && make install
