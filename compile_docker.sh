b1077c2e42b5ed180b8c34d490c82328a4c99590
gymreklab <melissa.gymrek@gmail.com>
2019-04-12 16:32:48 -0700

    updating compile instructions

--- compile_docker.sh
+++ compile_docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Build
-docker build -t gymreklab/cse185-course-docker .
+docker build --no-cache -t gymreklab/cse185-course-docker .
 
 # Test
 docker run -it gymreklab/cse185-course-docker /bin/bash
+
+# Push
+docker push gymreklab/cse185-course-docker
